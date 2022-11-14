e7d3dd0a3273c02b4c93816fe32ead2af9725522
Gregory Fournier <gregorykarlfournier@gmail.com>
2022-11-14 16:14:06 -0500

    Trying things out.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ repositories {
 dependencies {
     implementation("io.kweb:kweb-core:1.1.2.1")
     implementation("ch.qos.logback:logback-classic:1.4.4")
+    implementation("org.jsoup:jsoup:1.14.3")
 }
--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,12 +1,50 @@
 package kweb.template
 
 import kweb.*
+import kweb.html.BodyElement
+import kweb.state.KVar
+import kweb.util.json
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 fun main(args: Array<String>) {
-    Kweb(port = 16097, buildPage = {
+    Kweb(port = 16097) {
+        val site = "https://www.skidrowreloaded.com/"
+        val web = Jsoup.connect(site).get()
+        val posts = web.select(".post-excerpt").map {
+            it.child(1).child(0)
+        }.filterNotNull()
         doc.body.new {
-            h1().text("Replace Me!")
+            route {
+                path("/") { params ->
+                    val searchText = KVar("")
+                    input(type = InputType.text).value = searchText
+                    button().apply {
+                        text("Search")
+                        on.click {
+                            url.value = "/search/${searchText.value}"
+                        }
+                    }
+                    br()
+                    h1().text = searchText
+                    posts.forEach { post ->
+                        a(href = post.attr("href")) {
+                            img(
+                                mapOf(
+                                    "src" to post.child(0).attr("data-lazy-src").json
+                                )
+                            )
+                        }
+                    }
+                }
+                path("/search/{query}") { params ->
+                    val query = params.getValue("query")
+                    val newUrl = "https://www.skidrowreloaded.com/?s=${query.value.replace(" ", "+")}"
+                    val searchResult = Jsoup.connect(newUrl).get()
+                    println(searchResult)
+                }
+            }
         }
-    })
+    }
 }
 
